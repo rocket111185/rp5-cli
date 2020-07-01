ee5cd8e3354426a2b63fc908243fd599b2750431
D_MENT <rocket111185@gmail.com>
2020-07-01 22:33:05 +0300

    install.sh: resolving of possible problems

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo mkdir /usr/share/rp5-cli
-sudo cp rp5-cli /usr/bin
-sudo cp *.py /usr/share/rp5-cli
+printf 'Deleting the temporary files...\n';
+./uninstall.sh;
+printf 'Installation of needed packages...\n';
+sudo pip3 install bs4 lxml tabulate;
+printf 'Preparing the files...\n';
+sudo mkdir /usr/share/rp5-cli;
+sudo cp rp5-cli /usr/bin;
+sudo cp *.py /usr/share/rp5-cli;
+printf 'Done.\n'
