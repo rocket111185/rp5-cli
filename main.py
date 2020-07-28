6a2661675122b0acdc5d5b2274022a5227cc82e4
rocket111185 <rocket111185@gmail.com>
2020-07-28 13:23:28 +0300

    [main.py]: try another page, if current is invalid

--- main.py
+++ main.py
@@ -21,21 +21,28 @@ if len(city_name) < 1:
 
 # Make a query for Google
 query = 'rp5 ' + city_name
-urls = search(query, tld='com', lang='ua', start=0, stop=1)
+# You can control number of attempts, changing the `stop` value
+urls = search(query, tld='com', lang='ua', start=0, stop=10)
 
 # The default value for `url`
 url = None
-# `urls` is the iterable object, so
-# we need to do some iteration.
+# If it could reach the site, the `visited`
+# becomes true.
+visited = False
+
+# Go through pages.
 for link in urls:
     url = link
     # Show the name of web page to see
     # that Google returned correct link.
     print(unquote(urlparse(link).path))
+    # All the details in rp5_parser.py
+    try:
+        print_weather(url)
+        visited = True
+        break
+    except (IndexError, HTTPError):
+        print('Wrong page.')
 
-# All the details in rp5_parser.py
-try:
-    print_weather(url)
-except (IndexError, HTTPError):
-    print('''It is possible that the incorrect name of city was written.
-Try another.''')
+if not visited:
+    print('Unfortunately, it couldn\'t reach the needed cite.')
